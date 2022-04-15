90d63bdfa48a76e59ce02e2a0084c8eda2cb803e
tallner <ctallner@hotmail.com>
2022-04-15 22:16:31 +0200

    reset page added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import {GenerateTama} from './modules/GenerateNewTama.js'
 
+
 document.querySelector('#addTama').addEventListener('click', addTama);
+document.querySelector('#reset').addEventListener('click', reset);
 
 function addTama(){
+    console.log("add");
     GenerateTama();
+}
+
+function reset(){
+    location.reload();
 }
